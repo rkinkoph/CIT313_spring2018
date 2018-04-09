0cb91cce349e9ffe929d9c3d3f7215550354d44d
rkinkoph <rkinkoph@iupui.edu>
2018-04-08 23:37:28 -0400

    assignment 4
    added weather to nav

--- a4/views/elements/header.php
+++ a4/views/elements/header.php
@@ -56,6 +56,7 @@
               <li class="active"><a href="<?php echo BASE_URL?>">Home</a></li>
               <li><a href="<?php echo BASE_URL?>blog/">Blog</a></li>
               <li><a href="<?php echo BASE_URL?>members/">Members</a></li>
+              <li><a href="<?php echo BASE_URL?>weather/">Weather</a></li>
 
             </ul>
 
