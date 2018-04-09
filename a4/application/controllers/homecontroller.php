13df1763dc8534e2723429c45df482fab7853dc9
rkinkoph <rkinkoph@iupui.edu>
2018-04-08 23:02:41 -0400

    assign 4
    set errors to 1 to debug

--- a4/application/controllers/homecontroller.php
+++ a4/application/controllers/homecontroller.php
@@ -3,7 +3,7 @@
 class HomeController extends Controller{
 
 	public function index(){
-		$feed = "http://webhose.io/filterWebContent?token=c9547238-777b-4233-9a20-9f947b03497d&format=xml&sort=crawled&q=site%3Acnn.com";
+		$feed = "http://fox59.com/feed";
 		$rss = new RssDisplay($feed);
 
 		$number = 8;
